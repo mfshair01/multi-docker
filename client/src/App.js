68503b82ca4a408db37259709522ec1a5968efea
mfshair01 <mf.shair01@gmail.com>
2023-07-28 14:38:06 +0300

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            mohammad fareed
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
